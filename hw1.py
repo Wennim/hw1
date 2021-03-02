50a9c2cca8513b84ce9c7f3b17168adc6f06f684
Wennim <wennim1234@gmail.com>
2021-03-01 22:05:28 -0800

    add hw1.py

--- hw1.py
+++ hw1.py
@@ -15,40 +15,29 @@ with open(cwb_filename) as csvfile:
    mycsv = csv.DictReader(csvfile)
    header = mycsv.fieldnames
    for row in mycsv:
-       if row["TEMP"] != '-99.000' or row["TEMP"] != '-999.000':
-           
-        
+        if row["TEMP"] != '-99.000' and row["TEMP"] != '-999.000':
             data.append(row)
+
 #=======================================
 
 # Part. 3
 #=======================================
 # Analyze data depend on your group and store it to target_data like:
 # Retrive all data points which station id is "C0X260" as a list.
-C0A880_data = list(filter(lambda item: item['station_id'] == 'C0A880', data))
-C0F9A0_data = list(filter(lambda item: item['station_id'] == 'C0F9A0', data))
-C0G640_data = list(filter(lambda item: item['station_id'] == 'C0G640', data))
-C0R190_data = list(filter(lambda item: item['station_id'] == 'C0R190', data))
-C0X260_data = list(filter(lambda item: item['station_id'] == 'C0X260', data))
 
-C0A880_data=sorted(C0A880_data,key= lambda k:k["TEMP"],reverse= True )
-C0F9A0_data=sorted(C0F9A0_data,key= lambda k:k["TEMP"],reverse= True )
-C0G640_data=sorted(C0G640_data,key= lambda k:k["TEMP"],reverse= True )
-C0R190_data=sorted(C0R190_data,key= lambda k:k["TEMP"],reverse= True )
-C0X260_data=sorted(C0X260_data,key= lambda k:k["TEMP"],reverse= True )
+station_data=[]
+for station in ['C0A880','C0F9A0','C0G640','C0R190','C0X260']:
+  station_data.append(list(filter(lambda item: item['station_id'] == station, data)))
+  
+
+for i in range(5) :
+  station_data[i]=sorted(station_data[i],key= lambda k:k["TEMP"],reverse= True )
 
 target_data=[]
-target_data.append([C0A880_data[0]["station_id"],C0A880_data[0]["TEMP"]])
-target_data.append([C0F9A0_data[0]["station_id"],C0F9A0_data[0]["TEMP"]])
-target_data.append([C0G640_data[0]["station_id"],C0G640_data[0]["TEMP"]])
-target_data.append([C0R190_data[0]["station_id"],C0R190_data[0]["TEMP"]])
-target_data.append([C0X260_data[0]["station_id"],C0X260_data[0]["TEMP"]])
-
-# Retrive ten data points from the beginning.
-
+for i in range(5) :
+  target_data.append([station_data[i][0]["station_id"],station_data[i][0]["TEMP"]])
 
 #=======================================
-
 # Part. 4
 #=======================================
 # Print result
